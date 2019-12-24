18fb41b89ad4a80b82a7add6bd53f5a950c1af62
Rui Correia <rmgc31@gmail.com>
2019-12-24 18:38:18 +0000

    initial styling done

--- src/Components/AddProperty.jsx
+++ src/Components/AddProperty.jsx
@@ -4,6 +4,7 @@ import Axios from 'axios';
 import Alert from './Alert';
 
 import '../Styles/AddProperty.css';
+import img from '../img/add-property-bkg.jpg';
 
 class AddProperty extends Component {
   state = {
@@ -61,66 +62,71 @@ class AddProperty extends Component {
 
   render() {
     return (
-      <div className="add-property-container">
-        Add a Property
-        <form onSubmit={this.handleAddProperty}>
+      <div className="AddPropertyContainer">
+        <img src={img} />
+        <form onSubmit={this.handleAddProperty} className="add-property-form">
           <label>
             Brief Description:
             <input
               name="title"
               type="text"
+              className="description-box"
               value={this.state.fields.title}
               onChange={this.handleFieldChange}
               placeholder="Stunning 2 bedroom flat"
             />
           </label>
 
-          <label>
-            <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
-              <option value="Flat">Flat</option>
-              <option value="Detached">Detached</option>
-              <option value="Semi-Detached">Semi-Detached</option>
-              <option value="Terraced">Terraced</option>
-              <option value="End of Terrace">End of Terrace</option>
-              <option value="Cottage">Cottage</option>
-              <option value="Bungalow">Bungalow</option>
-            </select>
-          </label>
+          <div className="form-line-two">
+            <label>
+              <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
+                <option value="Flat">Flat</option>
+                <option value="Detached">Detached</option>
+                <option value="Semi-Detached">Semi-Detached</option>
+                <option value="Terraced">Terraced</option>
+                <option value="End of Terrace">End of Terrace</option>
+                <option value="Cottage">Cottage</option>
+                <option value="Bungalow">Bungalow</option>
+              </select>
+            </label>
 
-          <label>
-            <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
-              <option value="Manchester">Manchester</option>
-              <option value="Leeds">Leeds</option>
-              <option value="Liverpool">Liverpool</option>
-              <option value="Sheffield">Sheffield</option>
-            </select>
-          </label>
+            <label>
+              <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
+                <option value="Manchester">Manchester</option>
+                <option value="Leeds">Leeds</option>
+                <option value="Liverpool">Liverpool</option>
+                <option value="Sheffield">Sheffield</option>
+              </select>
+            </label>
+          </div>
 
-          <label>
-            No. Bedrooms:
-            <input
-              type="range"
-              min={1}
-              max={5}
-              steps={1}
-              name="bedrooms"
-              value={this.state.fields.bedrooms}
-              onChange={this.handleFieldChange}
-            />
-          </label>
+          <div className=".form-line-three">
+            <label>
+              No. Bedrooms:
+              <input
+                type="range"
+                min={1}
+                max={5}
+                steps={1}
+                name="bedrooms"
+                value={this.state.fields.bedrooms}
+                onChange={this.handleFieldChange}
+              />
+            </label>
 
-          <label>
-            No. Bathrooms:
-            <input
-              type="range"
-              min={1}
-              max={5}
-              steps={1}
-              name="bathrooms"
-              value={this.state.fields.bathrooms}
-              onChange={this.handleFieldChange}
-            />
-          </label>
+            <label>
+              No. Bathrooms:
+              <input
+                type="range"
+                min={1}
+                max={5}
+                steps={1}
+                name="bathrooms"
+                value={this.state.fields.bathrooms}
+                onChange={this.handleFieldChange}
+              />
+            </label>
+          </div>
 
           <label>
             Price:
@@ -129,6 +135,7 @@ class AddProperty extends Component {
               name="price"
               value={this.state.fields.price}
               onChange={this.handleFieldChange}
+              className="input-box"
             />
           </label>
 
@@ -140,6 +147,7 @@ class AddProperty extends Component {
               value={this.state.fields.email}
               onChange={this.handleFieldChange}
               placeholder="email@email.com"
+              className="input-box"
             />
           </label>
 
